699d2f8a25674e905eb7638c496e4d6d179b99f5
mushroomvoodoo <normeeboy@gmail.com>
2023-04-02 20:11:38 +0300

    end.

--- app/src/main/java/com/example/traflight/MainActivity.java
+++ app/src/main/java/com/example/traflight/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.traflight;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ConstraintLayout constraintlayout;
+    private TextView infoTextView;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        constraintlayout = findViewById(R.id.layout);
+        infoTextView = findViewById(R.id.textview1);
+
+    }
+
+    public void onRedButtonClick(View view) {
+        infoTextView.setText(R.string.redbutt);
+        constraintlayout.setBackgroundColor(getColor(R.color.redColor));
+    }
+    public void onYellowButtonClick(View view) {
+        infoTextView.setText(R.string.yellow);
+        constraintlayout.setBackgroundColor(getColor(R.color.yellowColor));
+    }
+    public void onGreenButtonClick(View view) {
+        infoTextView.setText(R.string.green);
+        constraintlayout.setBackgroundColor(getColor(R.color.greenColor));
     }
 }
